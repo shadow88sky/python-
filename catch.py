60204225b0a979ae3f933bc6f6470471da4156eb
shadow88sky <119136016@qq.com>
2015-08-19 16:11:27 +0800

    错误捕捉

---
+++ catch.py
@@ -0,0 +1,16 @@
+﻿#捕捉异常可以使用try/except语句。try/except语句用来检测try语句块中的错误，从而让except语句捕获异常信息并处理。如果你不想在异常发生时结束你的程序，只需在try里捕获它。语法：
+try:
+   fh = open("testfile", "w")
+   fh.write("This is my test file for exception handling!!")
+except IOError:
+   print "Error: can\'t find file or read data"
+else:
+   print "Written content in the file successfully"
+   fh.close()
+
+#try-finally 语句无论是否发生异常都将执行最后的代码。语法为：
+try:
+   fh = open("testfile", "w")
+   fh.write("This is my test file for exception handling!!")
+finally:
+   print "Error: can\'t find file or read data"
